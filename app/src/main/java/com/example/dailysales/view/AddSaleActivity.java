fc2819acdcded745b96e051152878643ddf346bb
bishal5917 <poudelb172@gmail.com>
2024-08-12 21:57:24 +0545

    home screen and add sales screen

--- app/src/main/java/com/example/dailysales/view/AddSaleActivity.java
+++ app/src/main/java/com/example/dailysales/view/AddSaleActivity.java
@@ -1,16 +1,28 @@
 package com.example.dailysales.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.example.dailysales.R;
+import com.example.dailysales.databinding.ActivityMainBinding;
 
 public class AddSaleActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_sale);
+        // Inflate the layout using Data Binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Set the toolbar
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Add your sale for today");
+        }
     }
 }
--- app/src/main/java/com/example/dailysales/view/MainActivity.java
+++ app/src/main/java/com/example/dailysales/view/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.dailysales.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.example.dailysales.R;
+import com.example.dailysales.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Inflate the layout using Data Binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Set the toolbar
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Daily Sales");
+        }
+        gotoAddSalesScreen();
+    }
+
+    public void gotoAddSalesScreen(){
+        binding.btnAdd.setOnClickListener(v->{
+            Intent intent = new Intent(MainActivity.this, AddSaleActivity.class);
+            startActivity(intent);
+        });
     }
 }
